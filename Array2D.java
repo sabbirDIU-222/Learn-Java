730af5b9161610c79616d35a745fa9ebcb2817c3
sabbirDIU-222 <57146729+sabbirDIU-222@users.noreply.github.com>
2020-01-19 12:57:42 +0600

    Add files via upload

--- Array2D.java
+++ Array2D.java
@@ -9,10 +9,7 @@ public class Array2D {
         
         int[][] A = new int[2][3];
         int[][] B = new int[2][3];
-        
-        
-        
-        // inputing loop
+        int[][] C = new int[2][3];
         
         System.out.println("take data for A:");
         
@@ -20,10 +17,9 @@ public class Array2D {
         {
             for(int col = 0; col<3; col++)
             {
-                System.out.printf("A[%d][%d] =",row,col);
+                System.out.printf("A[%d][%d] = ",row,col);
                 A[row][col] = input.nextInt();
-                
-      
+
             }
            
         }
@@ -44,59 +40,50 @@ public class Array2D {
            
         }
         
-        
-        // printing loop
-        
-    
+        System.out.print("A = ");
         for(int row=0;row<2;row++)
         {
             for(int col=0;col<3;col++)
             {
-                System.out.print(" "+A[row][col]);
+                System.out.print("\t "+A[row][col]);
             }
             System.out.println();
         }
         
+        System.out.println("\n");
+        
+        System.out.print("B = ");
         
           for(int row=0;row<2;row++)
         {
             for(int col=0;col<3;col++)
             {
-                System.out.print(" "+B[row][col]);
+                System.out.print("\t "+B[row][col]);
             }
             System.out.println();
         }
-      
+          
+          System.out.println();
+          System.out.print(" A+B = ");
+          
+          for(int row=0; row<2; row++)
+          {
+              for(int col=0; col<3; col++)
+              {
+                  C[row][col] = A[row][col] + B[row][col];
+                  System.out.print("\t " +  C[row][col]);
+              }
+              System.out.println();
+          }
+          
+        
+        
+        
+        
+        
+        
+        
+        
     }
  
-}                  
-                        ** output **
-                        
-
-
-run:
-take data for A:
-A[0][0] = 1
-A[0][1] = 2
-A[0][2] = 3
-A[1][0] = 4
-A[1][1] = 5
-A[1][2] = 6
-
-take data for B : 
-B[0][0] = 7
-B[0][1] = 8
-B[0][2] = 9
-B[1][0] = 5
-B[1][1] = 4
-B[1][2] = 3
-    
-    
-    
- 1 2 3
- 4 5 6
- 7 8 9
- 5 4 3
-BUILD SUCCESSFUL (total time: 18 seconds)
-
-
+}
