a331b438719a67e6a0ec585849b56273beca3595
sabbirDIU-222 <57146729+sabbirDIU-222@users.noreply.github.com>
2020-01-19 12:32:13 +0600

    Update Array2D.java

--- Array2D.java
+++ Array2D.java
@@ -10,6 +10,10 @@ public class Array2D {
         int[][] A = new int[2][3];
         int[][] B = new int[2][3];
         
+        
+        
+        // inputing loop
+        
         System.out.println("take data for A:");
         
         for(int row = 0; row<2 ;row++)
@@ -32,13 +36,16 @@ public class Array2D {
         {
             for(int col = 0;col<3;col++)
             {
-                System.out.printf("A[%d][%d] = ",row,col);
+                System.out.printf("B[%d][%d] = ",row,col);
                 B[row][col] = input.nextInt();
                 
                 
             }
            
         }
+        
+        
+        // printing loop
         
     
         for(int row=0;row<2;row++)
@@ -59,15 +66,37 @@ public class Array2D {
             }
             System.out.println();
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
+      
     }
  
-}
+}                  
+                        ** output **
+                        
+
+
+run:
+take data for A:
+A[0][0] = 1
+A[0][1] = 2
+A[0][2] = 3
+A[1][0] = 4
+A[1][1] = 5
+A[1][2] = 6
+
+take data for B : 
+B[0][0] = 7
+B[0][1] = 8
+B[0][2] = 9
+B[1][0] = 5
+B[1][1] = 4
+B[1][2] = 3
+    
+    
+    
+ 1 2 3
+ 4 5 6
+ 7 8 9
+ 5 4 3
+BUILD SUCCESSFUL (total time: 18 seconds)
+
+
