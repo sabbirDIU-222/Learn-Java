1d9f31b18c3de0efdfd06d3a731e37b3d319f4bb
sabbirDIU-222 <57146729+sabbirDIU-222@users.noreply.github.com>
2020-01-27 10:15:21 +0600

    Add files via upload

---
+++ Strintbuffer.java
@@ -0,0 +1,66 @@
+
+package string;
+
+import java.util.Scanner;
+
+
+public class Strintbuffer {
+    public static void main(String[] args) {
+        
+        String s1 = "md shimul islam ";
+        
+        StringBuffer my = new StringBuffer(s1);
+        
+        System.out.println("capacity : "+ my.capacity());
+        
+        my.append("sabbir ");
+        System.out.println(my);
+      
+        
+        my.append( 22);
+        System.out.println(my);
+        
+        my.setLength(6);
+        System.out.println(my);
+                
+        my.delete(1, 5);
+        System.out.println(my);
+        
+        
+        
+        
+        my.reverse();
+        System.out.println(my);
+        
+        
+       Scanner in = new Scanner(System.in);
+       String num ;
+       
+        System.out.println("takr");
+        num = in.next();
+        
+        
+        
+        StringBuffer mys = new StringBuffer(num);
+        
+        String check = mys.reverse().toString();
+        
+        if(check.equals(num))
+        {
+            
+            System.out.println(mys);
+            System.out.println("palindrome");
+        }
+        else
+        {
+            System.out.println("not palindrome");
+        }
+        
+      
+        
+        
+        
+        
+    }
+    
+}
